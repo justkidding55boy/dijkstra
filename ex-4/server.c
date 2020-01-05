3d7c409ca5f6e01de25db97f98c8d1b83ac73035
justkidding55boy <outlook.best116@gmail.com>
2020-01-05 15:47:28 +0900

    bind

--- ex-4/server.c
+++ ex-4/server.c
@@ -2,6 +2,7 @@
 #include <error.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 int main()
 {
@@ -11,5 +12,18 @@ int main()
         perror("socket");
     }
 
+	struct sockaddr_in skt;
+	memset(&skt, 0, sizeof skt);
+
+	memset(skt.sin_zero, 0, 8);
+	skt.sa_family_t = AF_INET;
+	//IPアドレスやポート番号を扱う場合にはhtonl()が必要
+	skt.sin_port = 0; //ポート番号
+	skt.sin_addr.a_addr = 0; //IP アドレス
+
+	if (bind(s, (struct sockaddr *)&skt, sizeof(skt)) < 0) {
+		perror("bind");
+		exit(1);
+	}
     return 0;
 }
